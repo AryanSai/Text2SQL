2e761f07bce9c4a649363430cf99c1dda9acfcb5
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-08-04 20:51:56 +0530

    new

--- Llama3.1.py
+++ Llama3.1.py
@@ -1,7 +1,7 @@
 from llama_cpp import Llama
 
 llm = Llama(
-      model_path="Models/Meta-Llama-3.1-8B-Instruct-IQ3_M.gguf",
+      model_path="Models/Meta-Llama-3.1-8B-Instruct-Q8_0.gguf",
       # n_gpu_layers=-1, # Uncomment to use GPU acceleration
       # seed=1337, # Uncomment to set a specific seed
       # n_ctx=2048, # Uncomment to increase the context window
